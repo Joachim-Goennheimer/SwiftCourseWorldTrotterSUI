71efdd4fa834139b22206cdfca5b4024c9537564
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-05-28 08:39:34 +0200

    Initial Commit

---
+++ WorldTrotterSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorldTrotterSUI
+//
+//  Created by Joachim Goennheimer on 28.05.20.
+//  Copyright © 2020 Joachim Goennheimer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
